a68c19852c07f2cb40c294dcfcd5e0b2a6483ebd
HariBhavPatel <haripatel042102@gmail.com>
2023-11-29 04:09:24 -0600

    Update digital_logic.lua
    Should be good scaled again

--- modules/digital_logic.lua
+++ modules/digital_logic.lua
@@ -63,188 +63,195 @@ function module.update()
 end
 
 function module.draw()
-    local scale_factor = 0.12
-    love.graphics.clear(CLEAR)
+    love.graphics.push()
+    local scale_factor = 0.20
+    love.graphics.setColor(CLEAR)
+    love.graphics.rectangle("fill",0,0,400,400)
 
     love.graphics.setColor(BLACK)
     love.graphics.setFont(love.graphics.newFont(smallFontSize))
-    love.graphics.print("Guess the logic gate",100)
+
     -- INPUT SWITCH
     love.graphics.setColor(WHITE)
     -- AND Done
     love.graphics.draw(green_input1_image, 0, 0, 0, scale_factor, scale_factor)
     love.graphics.setFont(love.graphics.newFont(smallFontSize))
-    love.graphics.print(" " .. (and_input1 and 1 or 0), 10, 15)
-    love.graphics.draw(green_input2_image, 0, 22, 0, scale_factor, scale_factor)
-    love.graphics.print("  " .. (and_input2 and 1 or 0), 5, 35)
+    love.graphics.print(" " .. (and_input1 and 1 or 0), 20, 28)
+    love.graphics.draw(green_input2_image, 0, 35, 0, scale_factor, scale_factor)
+    love.graphics.print("  " .. (and_input2 and 1 or 0), 15, 63)
+
 
     -- OR Done 
-    love.graphics.draw(green_input1_image, 0, 70, 0, scale_factor, scale_factor)
-    love.graphics.print(" " .. (nand_input1 and 1 or 0), 10, 85)
-    love.graphics.draw(green_input2_image, 0, 92, 0, scale_factor, scale_factor)
-    love.graphics.print(" " .. (nand_input2 and 1 or 0), 10, 105)
+    love.graphics.draw(green_input1_image, 0, 75, 0, scale_factor, scale_factor)
+    love.graphics.print(" " .. (nand_input1 and 1 or 0), 20, 105)
+    love.graphics.draw(green_input2_image, 0, 110, 0, scale_factor, scale_factor)
+    love.graphics.print(" " .. (nand_input2 and 1 or 0), 20, 140)
 
     -- NAND Done 
-    love.graphics.draw(green_input1_image, 0, 125, 0, scale_factor, scale_factor)
-    love.graphics.print(" " .. (or_input1 and 1 or 0), 10, 140)
-    love.graphics.draw(green_input2_image, 0, 148, 0, scale_factor, scale_factor)
-    love.graphics.print(" " .. (or_input2 and 1 or 0), 10, 160)
+    love.graphics.draw(green_input1_image, 0, 155, 0, scale_factor, scale_factor)
+    love.graphics.print(" " .. (or_input1 and 1 or 0), 20, 190)
+    love.graphics.draw(green_input2_image, 0, 190, 0, scale_factor, scale_factor)
+    love.graphics.print(" " .. (or_input2 and 1 or 0), 20, 220)
     
     -- NOR Done 
-    love.graphics.draw(green_input1_image, 0, 185, 0, scale_factor, scale_factor)
-    love.graphics.print(" " .. (nor_input1 and 1 or 0), 10, 200)
-    love.graphics.draw(green_input2_image, 0, 205, 0, scale_factor, scale_factor)
-    love.graphics.print(" " .. (nor_input2 and 1 or 0), 10, 220)
+    love.graphics.draw(green_input1_image, 0, 225, 0, scale_factor, scale_factor)
+    love.graphics.print(" " .. (nor_input1 and 1 or 0), 20, 255)
+    love.graphics.draw(green_input2_image, 0, 260, 0, scale_factor, scale_factor)
+    love.graphics.print(" " .. (nor_input2 and 1 or 0), 20, 295)
 
     -- LINE BETWEEN INPUT AND GATE
     love.graphics.setColor(BLACK)
-    love.graphics.line(50, 25, 150, 25) -- Line from Input 1 to the box
-    love.graphics.line(50, 45, 150, 45) -- Line from Input 2 to the box
-    love.graphics.line(50, 95, 150, 95) -- Line from Input 1 to the box
-    love.graphics.line(50, 113, 150, 113) -- Line from Input 2 to the box
-    love.graphics.line(50, 148, 150, 148) -- Line from Input 1 to the box
-    love.graphics.line(50, 172, 150, 172) -- Line from Input 2 to the box
-    love.graphics.line(50, 210, 150, 210) -- Line from Input 1 to the box
-    love.graphics.line(50, 230, 150, 230) -- Line from Input 2 to the box
+    love.graphics.line(75, 43, 150, 43) -- Line from Input 1 to the box
+    love.graphics.line(75, 75, 150, 75) -- Line from Input 2 to the box
+
+
+    love.graphics.line(75, 115, 150, 115) -- Line from Input 1 to the box
+    love.graphics.line(75, 148, 150, 148) -- Line from Input 2 to the box
+
+    love.graphics.line(75, 190, 150, 190) -- Line from Input 1 to the box
+    love.graphics.line(75, 227, 150, 227) -- Line from Input 2 to the box
+
+    love.graphics.line(75, 270, 150, 270) -- Line from Input 1 to the box
+    love.graphics.line(75, 300, 150, 300) -- Line from Input 2 to the box
 
     -- WHAT THE GATE LOOKS LIKE 
     local hexColor = "#eedc5b"
     local r, g, b = tonumber(hexColor:sub(2, 3), 16) / 255, tonumber(hexColor:sub(4, 5), 16) / 255, tonumber(hexColor:sub(6, 7), 16) / 255
 
     love.graphics.setColor(r, g, b)
-    love.graphics.rectangle("fill", 150, 15, 50, 50)
+    love.graphics.rectangle("fill", 150, 35, 50, 50)
     love.graphics.setFont(love.graphics.newFont(smallFontSize))
     love.graphics.setColor(WHITE)
-    love.graphics.print(box_1[current_text_box_index], 160, 15)
+    love.graphics.print(box_1[current_text_box_index], 155, 50)
 
     love.graphics.setColor(r, g, b)
-    love.graphics.rectangle("fill", 150, 78, 50, 50)
+    love.graphics.rectangle("fill", 150, 105, 50, 50)
     love.graphics.setFont(love.graphics.newFont(smallFontSize))
     love.graphics.setColor(WHITE)
-    love.graphics.print(box_2[current_text_box2_index], 160, 78)
+    love.graphics.print(box_2[current_text_box2_index], 155, 120)
 
     love.graphics.setColor(r, g, b)
-    love.graphics.rectangle("fill", 150, 140, 50, 50)
+    love.graphics.rectangle("fill", 150, 180, 50, 50)
     love.graphics.setFont(love.graphics.newFont(smallFontSize))
     love.graphics.setColor(WHITE)
-    love.graphics.print(box_3[current_text_box3_index], 160, 150)
+    love.graphics.print(box_3[current_text_box3_index], 155, 198)
 
     love.graphics.setColor(r, g, b)
-    love.graphics.rectangle("fill", 150, 200, 50, 50)
+    love.graphics.rectangle("fill", 150, 260, 50, 50)
     love.graphics.setFont(love.graphics.newFont(smallFontSize))
     love.graphics.setColor(WHITE)
-    love.graphics.print(box_4[current_text_box4_index], 160, 210)
+    love.graphics.print(box_4[current_text_box4_index], 155, 275)
 
     -- LINE BETWEEN GATE AND LINE 
     love.graphics.setColor(BLACK)
-    love.graphics.line(250, 35, 200, 35)
-    love.graphics.line(250, 100, 200, 100)
-    love.graphics.line(250, 170, 200, 170)
-    love.graphics.line(250, 225, 200, 225)
+    love.graphics.line(200, 55, 300, 55)
+    love.graphics.line(200, 125, 300, 125)
+    love.graphics.line(200, 205, 300, 205)
+    love.graphics.line(200, 285, 300, 285)
 
     -- LED SECTION
     love.graphics.setColor(and_output and YELLOW or BLACK)
-    love.graphics.circle("fill", 250, 35, 20)
+    love.graphics.circle("fill", 320, 60, 20)
 
     love.graphics.setColor(nand_output and YELLOW or BLACK)
-    love.graphics.circle("fill", 250, 100, 20)
+    love.graphics.circle("fill", 320, 125, 20)
 
     love.graphics.setColor(or_output and YELLOW or BLACK)
-    love.graphics.circle("fill", 250, 165, 20)
+    love.graphics.circle("fill", 320, 202, 20)
 
     love.graphics.setColor(nor_output and YELLOW or BLACK)
-    love.graphics.circle("fill", 250, 220, 20)
+    love.graphics.circle("fill", 320, 282, 20)
 
 
     -- SUBMIT BUTTON  
     love.graphics.setColor(BLUE)
-    love.graphics.rectangle("fill", 200, 300 , 80, 50)
+    love.graphics.rectangle("fill", 300,350 , 80, 50)
     love.graphics.setFont(love.graphics.newFont(mediumFontSize))
     love.graphics.setColor(WHITE)
-    love.graphics.print("Submit", 205, 305)
+    love.graphics.print("Submit", 305, 365)
 
-
+    love.graphics.setFont(love.graphics.newFont(12))
+    love.graphics.pop()
 end
 
 
 --* Handle Mouse Clicks --
 function module.mousepressed(x, y, button)
-    if button == 1 then -- Check if the left mouse button (button 1) was clicked
+    if ((button == 1) and (submitted ~= true)) then -- Check if the left mouse button (button 1) was clicked
         -- Check if the mouse click is within the boundaries of Input 1 text
-        if x > 0 and x < 50 and y > 0 and y < 30 then
+        if x > 55 and x < 90 and y > 30 and y < 60 then
             and_input1 = not and_input1 -- Toggle the state of Input 1
         end
+
         -- Check if the mouse click is within the boundaries of Input 2 text
-        if x > 0 and x < 50 and y > 40 and y < 55 then
+        if x > 55 and x < 93 and y > 65 and y < 100 then
             and_input2 = not and_input2 -- Toggle the state of Input 2
         end
 
         -- Check if the mouse click is within the boundaries of OR Input 1 text
-        if x > 0 and x < 50 and y > 80 and y < 100 then
+        if x > 55 and x < 95 and y > 100 and y < 135 then
             nand_input1 = not nand_input1 -- Toggle the state of OR Input 1
         end
 
         -- Check if the mouse click is within the boundaries of OR Input 2 text
-        if x > 0 and x < 50 and y > 112 and y < 120 then
+        if x > 55 and x < 98 and y > 142 and y < 170 then
             nand_input2 = not nand_input2 -- Toggle the state of OR Input 2
         end
 
         -- Check if the mouse click is within the boundaries of NAND Input 1 text
-        if x > 0 and x < 50 and y > 120 and y < 160 then
+        if x > 55 and x < 102 and y > 175 and y < 215 then
             or_input1 = not or_input1 -- Toggle the state of NAND Input 1
         end
 
-        -- Check if the mouse click is within the boundaries of NAND Input 2 text
-        if x > 0 and x < 50 and y > 170 and y < 180 then
+        -- -- Check if the mouse click is within the boundaries of NAND Input 2 text
+        if x > 55 and x < 95 and y > 220 and y < 250 then
             or_input2 = not or_input2 -- Toggle the state of NAND Input 2
         end
 
-        -- Check if the mouse click is within the boundaries of NOR Input 1 text
-        if x > 0 and x < 50 and y > 200 and y < 215 then
+        -- -- Check if the mouse click is within the boundaries of NOR Input 1 text
+        if x > 55 and x < 95 and y > 255 and y < 286 then
             nor_input1 = not nor_input1 -- Toggle the state of NOR Input 1
         end
 
-        -- Check if the mouse click is within the boundaries of NOR Input 2 text
-        if x > 0 and x < 50 and y > 220 and y < 235 then
-            -- nor_input2 = not nor_input2 -- Toggle the state of NOR Input 2
+        -- -- Check if the mouse click is within the boundaries of NOR Input 2 text
+        if x > 55 and x < 95 and y > 290 and y < 320 then
+            nor_input2 = not nor_input2 -- Toggle the state of NOR Input 2
         end
 
 
-        if x > 150 and x < 200 and y > 0 and y < 70 then
+        if x > 150 and x < 200 and y > 35 and y < 90 then
             current_text_box_index = current_text_box_index + 1
             if current_text_box_index > #box_1 then
                 current_text_box_index = 1 -- Reset to the first text
             end
         end
 
-        if x > 150 and x < 200 and y > 70 and y < 120 then
+        if x > 150 and x < 200 and y > 110 and y < 160 then
             current_text_box2_index = current_text_box2_index + 1
             if current_text_box2_index > #box_2 then
                 current_text_box2_index = 1 -- Reset to the first text
             end
         end
         
-        if x > 150 and x < 200 and y > 120 and y < 190 then
+        if x > 150 and x < 200 and y > 170 and y < 240 then
             current_text_box3_index = current_text_box3_index + 1
             if current_text_box3_index > #box_3 then
                 current_text_box3_index = 1 -- Reset to the first text
             end
         end
         
-        if x > 150 and x < 200 and y > 190 and y < 250 then
+        if x > 150 and x < 200 and y > 250 and y < 325 then
             current_text_box4_index = current_text_box4_index + 1
             if current_text_box4_index > #box_4 then
                 current_text_box4_index = 1 -- Reset to the first text
             end
         end
         
-        -- SUBMIT BUTTON CLICKABLE AREA 
-        if x > 200 and x < 300 and y > 270 and y < 400 then 
+        -- -- SUBMIT BUTTON CLICKABLE AREA 
+        if x > 300 and x < 380 and y > 350 and y < 400 then 
             -- CHECK IF all boxes are correct 
             submitted = true 
             module.checkAnswer()
-            
-
         end 
 
     end
