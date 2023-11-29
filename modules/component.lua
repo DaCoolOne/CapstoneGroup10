d4578ec8d02aec00045f18bfd7909f2d079a3a60
AdditionalVanilla <148937934+AdditionalVanilla@users.noreply.github.com>
2023-11-29 14:27:18 -0600

    Update component.lua

--- modules/component.lua
+++ modules/component.lua
@@ -1,47 +1,96 @@
-local component = {}
+local module = {}
+local looping_variable=1
 local togg_switch=0
-local tries = 2
 local success = 0
 local cursor = {}
+
 cursor.x = 0
 cursor.y = 0
 cursor.value=0.0
 local x = 100
 local y = 100
+local selectedres=0
 local circuit = {}
-function component.load()
-    circuit["v"]=2.4
-    circuit["i"]=4.8
-    circuit["r"]=0
+local switch = {}
+local resistorslot = {}
+local res1 = {}
+local res2 = {}
+function module.load()
+  circuit["v"]=BombInfo.num_batteries
+  circuit["i"]=circuit.v/.5
+  circuit["r"]=0
+  switch["x"]=200
+  switch["w"]=45
+  switch["y"]=100
+  switch["h"]=15    
+  resistorslot["x"]=300
+  resistorslot["w"]=45
+  resistorslot["y"]=200
+  resistorslot["h"]=15 
+  res1["x"]=50
+  res1["w"]=50
+  res1["y"]=350
+  res1["h"]=15
+  res1["r"]=2 
+  res2["x"]=150
+  res2["w"]=50
+  res2["y"]=350
+  res2["h"]=15
+  res2["r"]=.5  
 end
-function component.check_values()
+function module.check_values()
   if togg_switch ==1 then
     if circuit.v == circuit.i*circuit.r then
+      mark_solved()
       success =1
     end
     if circuit.v ~= circuit.i*circuit.r then
-      tries = tries-1
+      count_strike()
       togg_switch=0
     end
     togg_switch=0
   end
 end
-function component.update(dt)
+local buttonpressed
+function module.getButtonPressed(x,y)
+  if (x>=switch.x) and (x<=switch.x+switch.w) and (y>=switch.y) and (y<=switch.y+switch.h) then
+    buttonpressed=1
+  end
+  if (x>=res1.x) and (x<=res1.x+res1.w) and (y>=res1.y) and (y<=res1.y+res1.h) then
+    buttonpressed=3
+  end
+  if (x>=res2.x) and (x<=res2.x+res2.w) and (y>=res2.y) and (y<=res2.y+res2.h) then
+    buttonpressed=4
+  end
+  if (x>=resistorslot.x) and (x<=resistorslot.x+resistorslot.w) and (y>=resistorslot.y) and (y<=resistorslot.y+resistorslot.h) then
+    buttonpressed=2
+  end
+end
+function module.mousepressed(x,y)
+  if success==0 then
+    module.getButtonPressed(x,y)
+  end
+  
+end
+function module.mousereleased()
+  if (buttonpressed== 1) and (success==0) then
+    togg_switch=1
+    module.check_values()
+  end
+  if (buttonpressed== 2) and (success==0) then
+    circuit.r = selectedres
+  end
+  if (buttonpressed== 3) and (success==0) then
+    circuit.r = res1.r
+  end
+  if (buttonpressed== 4) and (success==0) then
+    circuit.r = res2.r
+  end
+    
+end
+function module.update(dt)
     print(dt)
-    if 1 <= x  and x <= 399 and 1 <= y and y <= 399 then
-      if love.keyboard.isDown("right") then
-        x = x + 60 * dt
-      end
-      if love.keyboard.isDown("left") then
-        x = x - 60 * dt
-      end
-      if love.keyboard.isDown("up") then
-        y = y - 60 * dt
-      end
-      if love.keyboard.isDown("down") then
-        y = y + 60 * dt
-      end
-    end
+    x , y = love.mouse.getPosition()
     if x < 1 then
       x=1
     end
@@ -54,64 +103,90 @@ function component.update(dt)
     if y > 399 then
       y=399
     end
-    if love.mousepressed() then
-      cursor.x , cursor.y = love.mouse.getPosition()
-      while love.mousereleased() == false do
-        local g = cursor.x
-      end
-    end
+    --if love.mousepressed() then
+      --cursor.x , cursor.y = love.mouse.getPosition()
+      --while love.mousereleased() == false do
+        --local g = cursor.x
+      --end
+    --end
+    --love.graphics.setColor( 1, 1, 1, 1 )
+    --love.graphics.print("apple",0,40)
+    --love.graphics.print(y,0,70)
+    --if love.mousepressed(x,y,1) then
+      --while looping_variable==1 do
+      --love.graphics.print(y,500,70)
+      --cursor.x = x
+      --cursor.y = y
+      --love.graphics.print(x,500,90)
+        --if love.mousereleased() then
+          --looping_variable=0
+        --end
+      --end
+      --looping_variable=1
+    --end
 end
-function component.draw()
-  if success == 0 and tries >= 0 then
+function module.drawstrikes()
+  
+  love.graphics.setColor( 1, 1, 0, 1 )
+  if BombInfo.strikes_remaining < 3 then
+    love.graphics.setColor(1,0,0,1)
+  end
+  if success==1 then
+    love.graphics.setColor(0,1,0,1)
+  end
+  love.graphics.rectangle("fill", 380, 380, 10, 10 )
+  love.graphics.setColor( 1, 1, 0, 1 )
+  if BombInfo.strikes_remaining < 2 then
+    love.graphics.setColor(1,0,0,1)
+  end
+  if success==1 then
+    love.graphics.setColor(0,1,0,1)
+  end
+  love.graphics.rectangle( "fill", 380, 360, 10, 10 )  
+end
+function module.drawcursor()
+  --love.graphics.setColor( 0, 0, 0, 1 )
+  --love.graphics.rectangle("fill", x,y, 3, 3)  
+end
+function module.draw()
+  if BombInfo.strikes_remaining >= 0 then
     love.graphics.setColor( 1, 1, 1, 1 )
     love.graphics.rectangle("fill", 0, 0, 400, 400)
-    love.graphics.setColor( 0, 0, 0, 1 )
-    love.graphics.rectangle("fill", x,y, 3, 3)
-    love.graphics.setColor( 1, 1, 0, 1 )
-    if tries < 2 then
-      love.graphics.setColor(1,0,0,1)
-    end
-    love.graphics.rectangle("fill", 380, 380, 10, 10 )
-    love.graphics.setColor( 1, 1, 0, 1 )
-    if tries < 1 then
-      love.graphics.setColor(1,0,0,1)
-    end
-    love.graphics.rectangle( "fill", 380, 360, 10, 10 )
+    module.drawstrikes()
+    module.drawcursor()
     love.graphics.setColor(0,0,0,1)
-    love.graphics.rectangle("line",100,100,200,200)
+    love.graphics.rectangle("line",100,100,200,200) -- circuit path
     love.graphics.setColor(0,0,1,1)
-    love.graphics.rectangle("fill",100,200,45,15)
-    love.graphics.rectangle("fill",200,300,45,15)
-    love.graphics.rectangle("fill",200,100,45,15)
+    love.graphics.rectangle("fill",100,200,45,15) -- voltage
+    love.graphics.rectangle("fill",200,300,45,15) -- amperage
+    love.graphics.rectangle("fill",200,100,45,15) -- switch
     love.graphics.setColor(0,1,0,1)
-    love.graphics.rectangle("fill",300,200,45,15)
+    love.graphics.rectangle("fill",300,200,45,15) --resistor slot
     love.graphics.setColor(1,0,1,1)
     love.graphics.rectangle("fill",50,350,50,15)
     love.graphics.rectangle("fill",150,350,50,15)
     love.graphics.setColor(1,1,1,1)
-    love.graphics.print(tostring(circuit.v) .. "V",100,200)
+    love.graphics.print(tostring("?") .. "V",100,200)
     love.graphics.print(tostring(circuit.i) .. "A",200,300)
     love.graphics.print(tostring(circuit.r) .. "Ohms",300,200)
     love.graphics.print("_/ _",200,100)
     love.graphics.print("2 Ohms",50,350)
     love.graphics.print(".5 Ohms",150,350)
-    --love.graphics.print(tostring(cursor.x), 500,30)
-    --love.graphics.print(tostring(cursor.y), 500,50)
+    love.graphics.print(tostring(x), 500,30)
+    love.graphics.print(tostring(y), 500,50)
     --love.graphics.print(tostring(cursor.value), 500,70)
     --love.graphics.print(tostring(circuit.r), 500,90)
-    component.selec()
-    component.check_values()
+    --module.selec()
+    --module.check_values()
   end
-  if success == 1 and tries >=0 then 
-    love.graphics.setColor(1,0,0,1)
-    love.graphics.print("A WINNER IS YOU!!!!", 185,120)
+  if success == 1 and BombInfo.strikes_remaining>=0 then 
+    --mark_solved()
+    --love.graphics.setColor(1,0,0,1)
+    --love.graphics.print("A WINNER IS YOU!!!!", 185,120)
   end
-  if tries < 0 then
-    love.graphics.setColor(1,0,0,1)
-    love.graphics.print("All Your Base Are Belong To Us!", 185,120)
-  end
+  
 end
-function component.selec()
+function module.selec()
   if 50<cursor.x and cursor.x<100 and 350<cursor.y and cursor.y<365 then
     cursor.value= 2.0
     cursor.x=0
@@ -134,4 +209,4 @@ function component.selec()
   end
 end
 
-return component
+return module
